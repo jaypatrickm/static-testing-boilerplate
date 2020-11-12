66b697ac0018d516fe69efe7614ce1d5e11fe053
JAYPATRICK MANALANSAN <jaypatrickm@gmail.com>
2020-11-12 15:29:55 -0800

    add gitignore and babel with yarn

---
+++ src/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const username = 'freddy'
+typeof username === 'strng'
+
+if (!'serviceWorke' in navigator) {
+    // you have an old browser;
+}
+
+const greeting = 'hello'
+console.log('${greeting} world!')
+[1,2,3].forEach(x => console.log(x))
