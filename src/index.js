3998e9a7db030adaa49c9eec15434416afd0dd71
JAYPATRICK MANALANSAN <jaypatrickm@gmail.com>
2020-11-12 17:33:30 -0800

    Added eslint and prettier. Create scripts for formatting and validating.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-'use strict'
-
 const username = 'freddy'
-typeof username === 'strng'
 
-if (!'serviceWorke' in navigator) {
-    // you have an old browser;
+typeof username === 'string'
+
+if (!('serviceWorker' in navigator)) {
+  // you have an old browser;
 }
 
 const greeting = 'hello'
-console.log('${greeting} world!')
-[1,2,3].forEach(x => console.log(x))
+console.log(`${greeting} world!`)
+;[1, 2, 3].forEach(x => console.log(x))
